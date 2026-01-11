63886d3b6df4458f2491e55ec849f79a7a055e58
IceGhostse <iceghostse@gmail.com>
2026-01-11 16:53:16 +0300

    fix: починил main с учётом изменений хранения информации о игроке

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
+import sys
+
 from src.core import *
 
 
-def start():
-    pass
+def game_start():
+    gamer = create_player()
 
-def game_loop():
+    return gamer
+
+def game_loop(player_data):
     is_fight = False
+    game_is_run = True
+    dungeon = create_dungeon()
 
-    while not is_fight:
-        is_fight = adventuring()
+    while game_is_run:
+        while not is_fight:
+            state_of_adventuring, new_position = adventuring(dungeon, player_data)
 
-    fight()
+            if state_of_adventuring == STATE_OF_ADVENTURING_FIGHT:
+                break
+
+            if state_of_adventuring == STATE_OF_ADVENTURING_EXIT:
+                exit(0)
+
+        fight(player_data)
+        if player_data[ENTITY_HP] <= 0:
+            print('GAME OVER')
+            sys.exit(0)
+        dungeon[new_position[0]][new_position[1]] = FLOOR_TILE
 
 def game_over():
     pass
 
 
-
 if __name__ == '__main__':
-    game_loop()
+    player = game_start()
+    game_loop(player)
