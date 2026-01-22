cbe3356659d5a73ad661ae96556b91243ff6c5f6
icy-beep <iceghostse@gmail.com>
2026-01-22 04:57:41 +0300

    добавил в листы сущностей элементы для хранения предметов и интоксикации

--- src/entities.py
+++ src/entities.py
@@ -1,7 +1,7 @@
 from src.constants import *
 player_name = ''
-player = [player_name, 100, 5, 10, 0.1]
-punk = [NAME_ENEMY_PUNK, 66, 5, 5, 0.1]
-synth_hound = [NAME_ENEMY_SYNTH_HOUND, 50, 7, 10, 0.1]
-glitch_butcher = [NAME_ENEMY_GLITCH_BUTCHER, 100, 5, 10, 0.3]
-psy_coder = [NAME_ENEMY_PSY_CODER, 75, 6, 10, 0.1]
+player = [player_name, 100, 5, 10, 0.1, 0, 0, 0]
+punk = [NAME_ENEMY_PUNK, 66, 5, 5, 0.1, 0]
+synth_hound = [NAME_ENEMY_SYNTH_HOUND, 50, 7, 10, 0.1, 0]
+glitch_butcher = [NAME_ENEMY_GLITCH_BUTCHER, 100, 5, 10, 0.3, 0]
+psy_coder = [NAME_ENEMY_PSY_CODER, 75, 6, 10, 0.1, 0]
