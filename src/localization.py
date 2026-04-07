5ab01a25561569a9d99191cebcefbfa7e044ed76
icy-beep <iceghostse@gmail.com>
2026-04-07 10:16:40 +0300

    feat: Добавил словарь для главного меню

---
+++ src/localization.py
@@ -0,0 +1,8 @@
+from src.constants import *
+
+MAIN_MENU = {
+    'player_input': '\n' + MAGENTA_TEXT_BRIGHT + '[SYSTEM@{PLAYER_NAME}]:' + RESET,
+    'decrypting_successful': GREEN_TEXT_BRIGHT + '[DECRYPTING SUCCESSFUL: OBJECT {MAIN_CHARACTER_NAME} ---> RESTORED]' + RESET,
+    'no_data_on_sector': RED_TEXT_BRIGHT + '[ERROR: NO DATA ON SECTOR' + RESET + MAGENTA_TEXT_BRIGHT + ' 0xxxx256]' + RESET
+
+}
